bab8a60a3ddfa2dfc7e03f4ec0da70b6262e8c38
Robin Hossen <hossenrobin215@gmail.com>
2026-04-22 15:40:31 +0600

    module-4 done

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from tasks.views import show_task
+from tasks.views import dashboard, manager_dashboard, user_dashboard,task
 
 
 urlpatterns = [
-    path('show-task/',show_task)
+    path('manager_dashboard/', manager_dashboard,),
+    path('user_dashboard/', user_dashboard),
+    path('dashboard/', dashboard),
+    path('task/', task),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -2,12 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home(request):
-    return HttpResponse("Hello, <h2>welcome to the Django Project!</h2>")
-def contact(request):
-    return HttpResponse("This is mera contact page ")
 
-def show_task(request):
-    return HttpResponse("show task page")
 
-    
+
+def manager_dashboard(request):
+    return render(request,"dashboard/manager_dashboard.html")
+
+def user_dashboard(request):
+    return render(request,'dashboard/user_dashboard.html')
+
+def dashboard(request):
+    return render(request,'dashboard/dashboard.html')
+
+def task(request):
+    context={
+        'message':'This is the task page'
+    }
+    return render(request,'task.html',context)
