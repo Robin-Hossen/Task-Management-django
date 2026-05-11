9cd8fc186b957f72adbdea07e4ceb0fc02ac19e4
Robin Hossen <hossenrobin215@gmail.com>
2026-05-11 22:51:19 +0600

    module 10.1 to 10.3

--- users/views.py
+++ users/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+from users.forms import  RegistrationForm
 
 # Create your views here.
+def sign_up(request):
+    if request.method == 'GET':
+        form = RegistrationForm()
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            # username=form.cleaned_data.get("username")
+            # password=form.cleaned_data.get("password1")
+            # confirm_password=form.cleaned_data.get("password2")
+            # if password == confirm_password:
+            #     User.objects.create_user(username=username,password=password)
+            # else:
+            #     print("Password and confirm password do not match")    
+            form.save()    
+
+    return render(request,"registration/register.html",{"form":form})
