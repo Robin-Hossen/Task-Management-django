110c8cac104f3eb33ed0030bce236c0f9b589ea5
Robin Hossen <hossenrobin215@gmail.com>
2026-06-05 23:21:05 +0600

    module 15.3 convert function to class based_view

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from tasks.views import dashboard, employee_dashboard, manager_dashboard,task,create_task, task_details, view_tasks,update_task,delete_task
+from tasks.views import dashboard, employee_dashboard, manager_dashboard,task,create_task, task_details, view_tasks,update_task,delete_task,CreateTask, TaskDetailModelForm
 
 
 urlpatterns = [
@@ -7,7 +7,8 @@ urlpatterns = [
     path('employee_dashboard/', employee_dashboard, name='user-dashboard'),
     #path('dashboard/', dashboard),
     path('task/', task),
-    path('create_task/',create_task,name='create_task'),
+    # path('create_task/',create_task,name='create_task'),
+    path('create_task/',CreateTask.as_view(),name='create_task'),
     path('view_tasks/',view_tasks),
     path('task/<int:task_id>/details/', task_details, name='task-details'),
     path('update_task/<int:id>/',update_task,name='update_task'),
--- tasks/views.py
+++ tasks/views.py
@@ -8,6 +8,16 @@ from django.db.models import Q,Max,Min,Count,Avg#Q object ar kaj hosse complex q
                             #and amra Q object er vitore | operator use kore OR condition create korte parbo and & operator use kore AND condition create korte parbo and ~ operator use kore NOT condition create korte parbo.
 from django.contrib.auth.decorators import login_required, permission_required,user_passes_test
 from users.views import is_admin
+from django.http import HttpResponse
+from django.views import View
+from django.utils.decorators import method_decorator
+
+
+
+
+#variables for list of decorators
+create_task_decorators=[login_required,permission_required('tasks.add_task', login_url='no-permission')]
+
 # Create your views here.
 
 def is_manager(user):
@@ -128,7 +138,7 @@ def create_task(request):
     
     
     task_form=TaskModelForm()# for Get 
-    task_detail_form=TaskDetailModelForm()# for Get
+    task_detail_form=TaskDetailModelForm()
 
     #Post ar jonne
     if request.method=="POST":
@@ -151,6 +161,36 @@ def create_task(request):
         "task_detail_form": task_detail_form
     }
     return render(request,"task_form.html",context)
+
+
+#create Task replace by class view
+
+@method_decorator(create_task_decorators, name='dispatch')#method_decorator ar kaj hosse class based view er method gulo te decorator apply kora jemon amra jodi class based view er get method ar vitore login_required and permission_required decorator apply korte chai tahole amra method_decorator use korechi and dispatch method ar vitore login_required and permission_required decorator apply korechi karon dispatch method ar vitore get, post, put, delete method gulo call hoy tai amra dispatch method ar vitore login_required and permission_required decorator apply korechi tahole get, post, put, delete method gulo te login_required and permission_required decorator apply hoye jabe
+
+class CreateTask(View):
+    def get(self,request,*args,**kwargs):
+        task_form=TaskModelForm()# for Get 
+        task_detail_form=TaskDetailModelForm()# for Get
+        context={
+            "task_form": task_form,
+            "task_detail_form": task_detail_form
+        }
+        return render(request,"task_form.html",context)
+    
+
+    def post(self,request,*args,**kwargs):
+        task_form=TaskModelForm(request.POST)# for Post
+        task_detail_form=TaskDetailModelForm(request.POST,request.FILES)# for Post
+        #print(form)
+        if task_form.is_valid() and task_detail_form.is_valid():#valid check na korle data clean korte dei na
+           task=task_form.save()#database a data entry ar jonne kaj hosse
+           task_detail=task_detail_form.save(commit=False)#commit false ar mane hocche task_detail form er data save hobe na karon amra task_detail form er data te task ar data save korte chai tai commit false use korechi
+           task_detail.task=task#task_detail form er task field te task_form er data save kortechi
+           task_detail.save()#task_detail form er data save kortechi
+
+           messages.success(request,"Task Added Successfully")#django te messages framework use kore amra user ke success message dekhate parbo jodi task successfully add hoy tahole amra user ke success message dekhate parbo and jodi task add na hoy tahole amra user ke error message dekhate parbo
+
+           return redirect('create_task')
 
 
 @login_required
